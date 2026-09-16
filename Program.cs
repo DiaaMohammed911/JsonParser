6427f4adb87bec12da8455c76cb05bef2b384376
DiaaMohammed <diaamoo306@gmail.com>
2026-09-16 17:00:32 +0300

    add additional test cases

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿
+Test("{\"name\":\"Diaa\",\"age\":22}");
+Test("[1, 2, 3]");
 Test("{\"a\": [1, 2], \"b\": {\"c\": true}}");
+Test("true");
+Test("null");
 static void Test(string json)
     {
         Console.WriteLine($"JSON: {json}");
